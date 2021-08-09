7605cb016d7abf7dfe943c19e60f1cb28aa253e0
Chen Keinan <Hen.Keinan@gmail.com>
2021-08-09 11:49:07 +0300

    fix: update eval command logic

--- eval/cmdeval.go
+++ eval/cmdeval.go
@@ -1,5 +1,6 @@
 package eval
 
+//CmdEvaluator interface expose one method to evaluate command with evalExpr
 type CmdEvaluator interface {
 	EvalCommand(commands []string, evalExpr string) CmdEvalResult
 }
@@ -7,17 +8,17 @@ type CmdEvaluator interface {
 type commandEvaluate struct {
 }
 
-func NewCmdEval() CmdEvaluator {
-	return &commandEvaluate{}
-}
-
+//New instansiate new command evaluator
 func New() CmdEvaluator {
 	return &commandEvaluate{}
 }
 
+//EvalCommand eval command with eval expr
+// accept command and evalExpr
+// return eval command result
 func (cv commandEvaluate) EvalCommand(commands []string, evalExpr string) CmdEvalResult {
 	commandParams := CommandParams(commands)
-	cmdExec := cmd{commandParams: commandParams, commandExec: commands, evalExpr: evalExpr ,command: NewShellExec()}
+	cmdExec := cmd{commandParams: commandParams, commandExec: commands, evalExpr: evalExpr, command: NewShellExec()}
 	cv.evalCommand(commands, cmdExec)
 	return CmdEvalResult{}
 }
@@ -28,9 +29,8 @@ func (cv commandEvaluate) evalCommand(commands []string, cmdExec cmd) {
 		res := cmdExec.execCommand(index, cmdTotalRes, make([]IndexValue, 0))
 		cmdTotalRes = append(cmdTotalRes, res)
 	}
-
 	// evaluate command result with expression
-	NumFailedTest := cmd.evalExpression(cmdTotalRes, len(cmdTotalRes), make([]string, 0), 0)
+	cmdExec.evalExpression(cmdTotalRes, len(cmdTotalRes), make([]string, 0), 0)
 }
 
 type CmdEvalResult struct {
