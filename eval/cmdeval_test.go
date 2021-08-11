afe6530fa629917c18bb6936a5a7bfe84ea90445
Chen Keinan <Hen.Keinan@gmail.com>
2021-08-11 07:51:05 +0300

    fix: add test for IN clause

--- eval/cmdeval_test.go
+++ eval/cmdeval_test.go
@@ -16,6 +16,7 @@ func TestEvalCommand(t *testing.T) {
 		{name: "two command and evalExpr match", cmd: []string{"ls /etc/hosts | awk -F \" \" '{print $1}' |awk 'FNR <= 1'", "ls /etc/group | awk -F \" \" '{print $1}' |awk 'FNR <= 1'"}, evalExpr: "'$0' == '/etc/hosts'; && '$1' == '/etc/group';", want: true},
 		{name: "two command and evalExpr do one not match", cmd: []string{"ls /etc/hosts | awk -F \" \" '{print $1}' |awk 'FNR <= 1'", "ls /etc/group | awk -F \" \" '{print $1}' |awk 'FNR <= 1'"}, evalExpr: "'$0' == '/etc/hosts'; && '$1' == '/etc/group1';", want: false},
 		{name: "two command and evalExpr do both not match", cmd: []string{"ls /etc/hosts | awk -F \" \" '{print $1}' |awk 'FNR <= 1'", "ls /etc/group | awk -F \" \" '{print $1}' |awk 'FNR <= 1'"}, evalExpr: "'$0' == '/etc/hosts1'; && '$1' == '/etc/group1';", want: false},
+		{name: "command and evalExpr in clause match", cmd: []string{"ls /etc | awk -F \" \" '{print $1}' |awk 'FNR <= 2'"}, evalExpr: "'$0' IN ('afpovertcp.cfg','aliases')", want: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
