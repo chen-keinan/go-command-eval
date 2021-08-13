73e6bbdd2f8031ce4b65a962fb290f75450af157
Chen Keinan <Hen.Keinan@gmail.com>
2021-08-13 21:12:36 +0300

    test: eval expr

--- eval/cmd_test.go
+++ eval/cmd_test.go
@@ -1,6 +1,9 @@
 package eval
 
-import "testing"
+import (
+	"github.com/chen-keinan/go-command-eval/utils"
+	"testing"
+)
 
 func TestCommandParams(t *testing.T) {
 	tests := []struct {
@@ -8,9 +11,9 @@ func TestCommandParams(t *testing.T) {
 		cmd  []string
 		want map[int][]string
 	}{
-		{name: "two command and one param", cmd: []string{" aaa", "bb #1"}, want: map[int][]string{1:{"1"}}},
-		{name: "two command and 2 params on 2 commands", cmd: []string{" aaa", "bb #1", "cc #2"}, want: map[int][]string{1:{"1"},2:{"2"}}},
-		{name: "two command and 2 params on one command", cmd: []string{" aaa", "bb #1", "cc #1 #2"}, want: map[int][]string{1:{"1"},2:{"1","2"}}},
+		{name: "two command and one param", cmd: []string{" aaa", "bb #1"}, want: map[int][]string{1: {"1"}}},
+		{name: "two command and 2 params on 2 commands", cmd: []string{" aaa", "bb #1", "cc #2"}, want: map[int][]string{1: {"1"}, 2: {"2"}}},
+		{name: "two command and 2 params on one command", cmd: []string{" aaa", "bb #1", "cc #1 #2"}, want: map[int][]string{1: {"1"}, 2: {"1", "2"}}},
 		{name: "two command no params", cmd: []string{" aaa", "bb ", "cc"}, want: map[int][]string{}},
 	}
 	for _, tt := range tests {
@@ -31,6 +34,30 @@ func TestCommandParams(t *testing.T) {
 						t.Errorf("CommandParams() = %v, want %v", got, tt.want)
 					}
 				}
+			}
+		})
+	}
+}
+
+func TestEvalExpression(t *testing.T) {
+
+	tests := []struct {
+		name        string
+		commandRes  []string
+		commResSize int
+		testFailure int
+		evalExpr    string
+		want        int
+		wantErr     error
+	}{
+		{name: "one command res and one param", commandRes: []string{"/etc/hosts"}, commResSize: 1, testFailure: 0, evalExpr: "'$0' == '/etc/hosts'", want: 0, wantErr: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmdEval := cmd{cmdExprBuilder: utils.UpdateCmdExprParam, evalExpr: tt.evalExpr}
+			got, err := cmdEval.evalExpression(tt.commandRes, tt.commResSize, make([]string, 0), tt.testFailure)
+			if tt.want != got && err != tt.wantErr {
+				t.Errorf("evalExpression() = %v, want %v", got, tt.want)
 			}
 		})
 	}
