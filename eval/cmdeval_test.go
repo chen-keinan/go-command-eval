afd6bcdc12a27ad1c6698e24172671af641b9472
Chen Keinan <Hen.Keinan@gmail.com>
2021-08-14 22:23:42 +0300

    test: exec with params

--- eval/cmdeval_test.go
+++ eval/cmdeval_test.go
@@ -19,11 +19,13 @@ func TestEvalCommand(t *testing.T) {
 		{name: "command and evalExpr in clause match", cmd: []string{"ls /etc | awk -F \" \" '{print $1}' |awk 'FNR <= 2'"}, evalExpr: "'$0' IN ('afpovertcp.cfg','aliases')", want: true},
 		{name: "command and evalExpr in clause notmatch", cmd: []string{"ls /etc | awk -F \" \" '{print $1}' |awk 'FNR <= 2'"}, evalExpr: "'$0' IN ('afpovertcp.cfg1','aliases')", want: false},
 		{name: "single command and evalExpr match", cmd: []string{"ls /etc/hosts | awk -F \" \" '{print $1}' |awk 'FNR <= 1'", "grep local #0| awk -F \"127.0.0.1\" '{print $1}' |awk 'FNR <= 1' | awk -F \" \" '{print $3}' |awk 'FNR <= 1'", "grep local #0 | awk -F \"localhost\" '{print $2}' |awk 'FNR <= 2'|grep tservice | awk -F \" \" '{print $2}'"}, evalExpr: "'$0' == '/etc/hosts'; && '$1' == 'is'; && '$2' == 'tservice';", want: true},
+		{name: "single command and evalExpr match ok", cmd: []string{"stat -f %A /etc/hosts"}, evalExpr: "$0 < 776", want: true},
+		{name: "single command and evalExpr match bad", cmd: []string{"stat -f %A /etc/host 2> /dev/null"}, evalExpr: "$0 < 776", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := res.EvalCommand(tt.cmd, tt.evalExpr); got.Match != tt.want {
-				t.Errorf("EvalCommand() = %v, want %v", got, tt.want)
+				t.Errorf("EvalCommand() = %v, want %v err %v", got, tt.want, got.Error)
 			}
 		})
 	}
